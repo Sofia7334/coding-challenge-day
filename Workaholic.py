a4590e8671d48b6f440347c84446e22d7e257ed5
Sofia <samunoz.flower@gmail.com>
2025-12-09 13:10:08 +0000

    Complete: Problem 1 — Workaholic

---
+++ Workaholic.py
@@ -0,0 +1,6 @@
+# Problem 1 — Workaholic
+
+# Input: Single integer N (1<N<10) containing the number of hours Denise worked for today.
+N = int(input("Input the number of hours Denise worked for today between 1 and 10: "))
+sleep = str(abs(N-24))
+print("Output: " + sleep)
