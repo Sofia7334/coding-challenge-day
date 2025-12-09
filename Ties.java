e1c9151c3cd832c4d56460fcd3ed901748c7587c
Sofia <samunoz.flower@gmail.com>
2025-12-09 13:36:41 +0000

    Attempt: Problem 2 — Ties in a Game

---
+++ Ties.java
@@ -0,0 +1,9 @@
+// Problem 2 — Ties in a Game
+
+// class Player {
+//      int points;
+//      int matches;
+// public Player (String name, int matches, int points)
+//      this.name = name;
+//      this.matches = matches;
+//      this.points = points;
